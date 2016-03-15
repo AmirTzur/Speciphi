94a54d8a2dd2f38d558572c09d42026e2fe390c9
Amir Tzur <tzuramir@gmail.com>
2016-03-15 06:48:28 +0200

    Add swipe left-right functionality for the products feature

--- static_in_pro/our_static/js/index_products.js
+++ static_in_pro/our_static/js/index_products.js
@@ -3,6 +3,7 @@
  */
 
 $(document).ready(function () {
+
     //Temp fix for the dynamic gutters between the product divs
     $('#product-list').find('div').each(function () {
         $(this).insertAfter($("#product-list div:last-child"))
@@ -15,4 +16,22 @@ $(document).ready(function () {
     $('#right-control-btn').click(function () {
         $("#product-list div:last-child").insertBefore($("#product-list div:first-child"));
     });
+    (function(){
+        var swipe;
+        var products_list = document.getElementById('products-wrapper');
+        var products = new Hammer(products_list);
+        products.on("panleft panright", function(ev){
+            swipe = ev.type;
+        });
+        products.on("panend", function(ev){
+            if (swipe == "panleft"){
+                $("#product-list div:first-child").insertAfter($("#product-list div:last-child"));
+            }
+            if (swipe == "panright"){
+                $("#product-list div:last-child").insertBefore($("#product-list div:first-child"));
+            }
+        });
+    }());
+
+
 });
