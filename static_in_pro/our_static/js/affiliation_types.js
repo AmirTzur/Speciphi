3c0bd3714885c5986d8e0c32af3d5817831d90f7
Elad Dan <eladdan88@gmail.com>
2016-05-02 02:43:54 +0300

    press of button trigger checkbox change

--- static_in_pro/our_static/js/affiliation_types.js
+++ static_in_pro/our_static/js/affiliation_types.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $('div#types_display div:nth-child(1)').css('display', 'block');
     // fix padding
     $('div#mobile_types_list ul li:first-child').css('padding-top', '15px');
-    // click on list change displayed type # change to mobile only
+    // click on list will change displayed type # change to mobile only
     $('div#mobile_types_list ul li').on('click', function () {
         // get pressed type
         var pressed_type = $(this).context.innerText;
@@ -17,33 +17,37 @@ $(document).ready(function () {
         }).css('display', 'none');
         // change pressed type css to display: block
         $('div#types_display').children().filter(function () {
-            return $(this).attr('id') == pressed_type;
+            return $(this).prop('id') == pressed_type;
         }).css('display', 'block');
     });
     // on checkbox changed, set or remove V #change to mobile only
     $(':checkbox').change(function () {
-        var checked_type = $(this).attr('name');
-        console.log(checked_type);
+        var checked_type = $(this).prop('name');
         if ($(this).is(":checked")) {
             // set V
-            console.log('checked');
             $('div#mobile_types_list ul').children().filter(function () {
                 return $(this).context.innerText == checked_type;
             }).children().css('display', 'inline');
         }
         else {
             // remove V
-            console.log('unchecked');
             // inner <i> adds a weird space at beginning of innerText - remove it
             $('div#mobile_types_list ul').children().filter(function () {
                 var mobile_list_type = $(this).context.innerText;
                 if (!(/^[0-9a-zA-Z]+$/.test(mobile_list_type.charAt(0)))) {
                     mobile_list_type = mobile_list_type.substring(1);
                 }
-                console.log(mobile_list_type + ',' + checked_type);
                 return mobile_list_type == checked_type;
             }).children().css('display', 'none');
         }
     });
-
+    // check or uncheck checkbox with a press of button
+    $('div#types_display div button').click(function () {
+        if ($(this).parent().children('input[type=checkbox]').is(':checked')) {
+            $(this).parent().children('input[type=checkbox]').prop('checked', false).change();
+        }
+        else {
+            $(this).parent().children('input[type=checkbox]').prop('checked', true).change();
+        }
+    });
 });
