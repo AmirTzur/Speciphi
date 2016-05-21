31d9a959761166a64989de6efc290106ecaf11c3
Elad Dan <eladdan88@gmail.com>
2016-05-22 01:04:08 +0300

    desktop: reverse use_level_buttons order. modile: back to default order

--- static_in_pro/our_static/js/application_uses.js
+++ static_in_pro/our_static/js/application_uses.js
@@ -60,7 +60,6 @@ $(document).ready(function () {
                 // get pressed level of use
                 var selected_use_level = $(this).prop('value');
                 $(this).parent().children('span').each(function () {
-                    console.log(this);
                     // update description div # desktop screens
                     if ($(this).prop('id') == 'description_' + selected_use_level) {
                         $('#description_container').html($(this).html());
@@ -79,7 +78,7 @@ $(document).ready(function () {
             });
             // show first use
             $('div#uses_display div').first().css('display', 'inline-block');
-            // if was on desktop switch back default use display
+            // if was on desktop switch back default use display and buttons order
             if ($('div#uses_display').children().first().children().first().hasClass('use_levels')) {
                 $('div#uses_display').children().each(function () {
                     $(this).children('div').last().removeClass('use_name');
@@ -99,24 +98,37 @@ $(document).ready(function () {
                         }
                     });
                 });
+                // change use_levels buttons order to default
+                $('#uses_display').children('div.use_display').each(function () {
+                    $($(this).children('div.use_levels').children('button').get().reverse()).each(function (index) {
+                        $(this).parent('div').append(this);
+                    });
+                });
             }
 
         } //end mobile screens
-
         // desktop screens
         else if (window.matchMedia("(min-width: 992px)").matches) {
             // display uses in line
             $('div#uses_display').children().css('display', 'inline-block');
-            // hide description text
-            $('span.description_text').css('display', 'none');
+
             // remove use_circle class and show name at the bottom
             $('div#uses_display').children().each(function () {
                 $(this).children('div').first().removeClass('use_circle');
                 $(this).children('div').first().addClass('use_name');
                 $(this).children('div').first().appendTo(this);
             });
+            // hide description text
+            $('span.description_text').css('display', 'none');
             // add new description container
-            $('div#uses_wrapper form').prepend('<div id="description_container">text text text text text text text text text</div>');
+            $('div#uses_wrapper form').prepend('<div id="description_container"></div>');
+            // change use_levels buttons order
+            $('#uses_display').children('div.use_display').each(function () {
+                // iterate buttons in reverse order
+                $($(this).children('div.use_levels').children('button').get().reverse()).each(function (index) {
+                    $(this).parent('div').append(this);
+                });
+            });
         }//end desktop screens
     }// end responsive query
 
@@ -151,15 +163,17 @@ $(document).ready(function () {
         var selected_use_level = $(that).prop('value');
         // update data-brand (where we keep algorithm choice value)
         $(that).parent().parent().attr('data-brand', selected_use_level);
-        // show matched description
-        $(that).parent().parent().children('span').each(function () {
-            if ($(this).prop('id') == 'description_' + selected_use_level) {
-                $(this).css('display', 'block');
-            }
-            else {
-                $(this).css('display', 'none');
-            }
-        });
+        // show matched description #mobile only
+        if (window.matchMedia("(max-width: 991px)").matches) {
+            $(that).parent().parent().children('span').each(function () {
+                if ($(this).prop('id') == 'description_' + selected_use_level) {
+                    $(this).css('display', 'block');
+                }
+                else {
+                    $(this).css('display', 'none');
+                }
+            });
+        }
     }
 
 
