0cf398f27b4d77f35be1051ae0170aa2cd976db7
Amir Tzur <tzuramir@gmail.com>
2016-05-22 11:48:57 +0300

    Edit upate_deals vars and add ScrollTop animation on open/close

--- static_in_pro/our_static/js/results_feature.js
+++ static_in_pro/our_static/js/results_feature.js
@@ -27,43 +27,34 @@ $(document).ready(function(){
     $('#open-btn').click(function(){
         $('#close-mode').css('display', 'none');
         $('#open-mode').css('display', 'block');
-        $('html, body').scrollTop($(document).height());
+        $('html, body').animate({scrollTop : $(document).height()},500);
     });
     $('#close-btn').click(function(){
-        $('#open-mode').css('display', 'none');
-        $('#close-mode').css('display', 'block');
+        $('html, body').animate({scrollTop : 0},200, function(){
+            $('#open-mode').css('display', 'none');
+            $('#close-mode').css('display', 'block');
+        });
     });
     
 });
 
 function update_deals(total_results, offers) {
-    var deal, deal_offers, deal_url, deal_img,
-        deal_price, deal_brand, deal_model;
+    var deal, deal_offers;
     var deal_index = 0;
-    console.log(total_results);
     // update total results
     $('#total-results span:nth-child(1)').text(total_results);
     // update deals details
-    console.log(offers);
     $('#deals-list a').each(function(){
         deal = offers[deal_index];
         deal_offers = deal["offers"];
-        deal_url = deal_offers[0]["deal_url"];
-        deal_price = deal_offers[0]["price"];
-        deal_brand = deal["brand"];
-        deal_model = deal["model"];
-        deal_img = deal["image_url"];
         // change deal url
-        $(this).attr('href', deal_url);
+        $(this).attr('href', deal_offers[0]["deal_url"]);
         // change deal price
-        $(this).find('.item-details span:nth-child(2)').text('$' + deal_price);
+        $(this).find('.item-details span:nth-child(2)').text('$' + deal_offers[0]["price"]);
         // change deal brand and model
-        $(this).find('.item-details span:nth-child(3)').text(deal_brand + ' ' + deal_model);
+        $(this).find('.item-details span:nth-child(3)').text(deal["brand"] + ' ' + deal["model"]);
         // change deal image url
-        $(this).find('img').attr('src', deal_img);
-        console.log(deal_index);
-        console.log(deal_url);
-        console.log(deal_img);
+        $(this).find('img').attr('src', deal["image_url"]);
         deal_index += 1;
     });
 }
