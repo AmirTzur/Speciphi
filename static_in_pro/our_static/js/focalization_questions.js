7e53036435efafc53e0f69b5e2fb56c47cb84235
Elad Dan <eladdan88@gmail.com>
2016-06-23 22:44:14 +0300

    disable answer button functionality on side mode, added separator line, subject display

--- static_in_pro/our_static/js/focalization_questions.js
+++ static_in_pro/our_static/js/focalization_questions.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     var desktop_flag = false;
+
     // show and save first question
     $('div.questions').first().children('div.question').first().css('display', 'inline-block');
     var $current_question = $('div.questions').first().children('div.question').first();
@@ -7,63 +8,13 @@ $(document).ready(function () {
     $current_question.parent('div.questions').children('div.question').each(function (index) {
         $('#question_indicators div:nth-child(' + parseInt(index + 1) + ')').css('display', 'inline-block');
     });
-    // answer's click
-    $('button.answer').click(function (event) {
-        // don't send form
-        event.preventDefault();
-        // if not selected before - increase select box number of questions answered
-        var increase_answered = true;
-        $(this).parent('div.answers').children('button.answer').each(function () {
-            // if already selected
-            if ($(this).attr('value') == '1') {
-                increase_answered = false;
-            }
-        });
-        // update button pressed indicator
-        // color 'on & off' state
-        if ($(this).attr('value') == '0') {
-            // cancel selected brothers and color them 'off'
-            $(this).siblings('button').attr('value', '0');
-            $(this).siblings('button').css({
-                'background-color': 'rgb(240, 240, 240)',
-                'font-weight': 'normal'
-            });
-            // check and color 'on'
-            $(this).attr('value', '1');
-            $(this).css({
-                'background-color': 'rgb(220, 220, 220)',
-                'font-weight': 'bold'
-            });
 
-        }
-        else if ($(this).attr('value') == '1') {
-            // un check and color 'off'
-            $(this).attr('value', '0');
-            $(this).css({
-                'background-color': 'rgb(240, 240, 240)',
-                'font-weight': 'normal'
-            });
-        }
-
-        // if no selection at the end - reduce at select box the number of questions answered
-        var decrease_answered = true;
-        $(this).parent('div.answers').children('button.answer').each(function () {
-            if ($(this).attr('value') == '1') {
-                decrease_answered = false;
-            }
-        });
-
-        // update questions number on select box
-        if (increase_answered || decrease_answered) {
-            var $current_subject = $('select#questions_list option[value=' + $current_question.parent('div').prop('id').substring('questions_'.length) + ']');
-            UpdateAnsweredNumber($current_subject, increase_answered, true);
-            if (window.matchMedia("(min-width: 992px)").matches) {
-                $current_subject = $('button#questions_circle_' + $current_question.parent('div').prop('id').substring('questions_'.length));
-                UpdateAnsweredNumber($current_subject, increase_answered, false);
-            }
-        }
-        // IMPLEMENT: sent taltul question and answer id using ajax
-    });
+    // button clicks bind
+    // prevent form submission
+    $('button.answer').on('click', PD);
+    // increase / decrease subject and option number and style button
+    $('button.answer').on('click', AnswerClick);
+    // IMPLEMENT: sent taltul question and answer id using ajax
 
     // display questions according to select chose
     $('select').on('change', function () {
@@ -117,8 +68,6 @@ $(document).ready(function () {
                 $current_question.siblings('div.question').css('display', 'none');
                 // select last displayed questions
                 $("select#questions_list").val($current_question.parent('div.questions').prop('id').substring('questions_'.length));
-                // remove line separator
-                $('#line_separator').remove();
                 // remove side question style
                 $('div.question').removeClass('side_question');
                 $('div.answers').css('display', '');
@@ -130,6 +79,7 @@ $(document).ready(function () {
         // desktop screens
         else if (window.matchMedia("(min-width: 992px)").matches) {
             desktop_flag = true;
+
             // on first desktop entry
             if (!$('#desktop_questions_list').length) {
                 // create questions row container
@@ -149,12 +99,11 @@ $(document).ready(function () {
                         "</div>"
                     )
                 });
-                // questions circle action
-                $('button.questions_circle').click(function (event) {
-                    event.preventDefault();
-                });
+                // prevent questions circle action
+                $('button.questions_circle').on('click', PD);
+
                 // display pressed subject questions (only if user didn't press on displayed subject)
-                $('div.questions_desktop_container').click(function () {
+                $('div.questions_desktop_container').on('click', function () {
                     var pressed_subject_id = $(this).children('button').prop('id').substring('questions_circle_'.length);
                     var displayed_subject_id = $current_question.parent('div').attr('id').substring('questions_'.length);
                     if (pressed_subject_id != displayed_subject_id) {
@@ -165,15 +114,30 @@ $(document).ready(function () {
                         StyleQuestions($current_question);
                     }
                 });
+                // add line separator
+                $('div#questions_holder').before('<hr id="line_separator"/>');
 
+                // add questions subject display
+                $('hr#line_separator').before(
+                    "<div id='questions_desktop_container_display'>" +
+                    "<span id='questions_subject_display' style='display: block;'>" +
+                    "Some Subject" +
+                    "</span>" +
+                    "<div id='questions_circle_display' style='display: block;'>" +
+                    '1/3' +
+                    "</div>" +
+                    "</div>"
+                );
             } // end first desktop entry
-            // add line separator
-            $('div#questions_holder').before('<hr id="line_separator"/>');
+
+
             // show current question brothers
             $current_question.parent('div.questions').children('div.question').css('display', 'inline-block');
             StyleQuestions($current_question);
-            // mark current subject CONTINUE IMPLEMENT
-            $('div#questions_desktop_container_' + $current_question.parent('div.questions').prop('id').substring('questions_'.length)).children().css('color', 'black');
+
+            // mark current subject CONTINUE IMPLEMENT####################################
+            // $('div#questions_desktop_container_' + $current_question.parent('div.questions').prop('id').substring('questions_'.length)).children().css('color', 'black');
+
             // update number of answered questions (from mobile select box)
             $('select#questions_list').children('option').each(function () {
                 $('button#questions_circle_' + $(this).val()).text($(this).text()[1] + $(this).text()[2] + $(this).text()[3]);
@@ -192,10 +156,8 @@ $(document).ready(function () {
         WidthChange();
     }
 
-
     function UpdateAnsweredNumber($subject, increase, mobile) {
         var inner_text = $subject.text();
-
         if (mobile) {
             if (increase) {
                 // get text of current questions subject (select option)
@@ -265,7 +227,6 @@ $(document).ready(function () {
             }
         }
         $subject.text(inner_text);
-
     }
 
     function StyleQuestions($current) {
@@ -288,29 +249,85 @@ $(document).ready(function () {
                 StyleQuestion($questions.last());
                 break;
             default:
-                // do nothing
+                $('div.questions').css('marginLeft', '');
                 break;
-
         }
+
         function StyleQuestion($question) {
             $question.addClass('side_question');
             $question.children('div.answers').addClass('side_answers');
-            // check an question was answered
+            // if question was answered - show only answer and disable button click
             var hided_siblings = false;
             $($question.children('div.answers')).children('button').each(function () {
                 if ($(this).attr('value') == '1') {
                     // hide brothers
                     $(this).siblings('button').css('display', 'none');
                     // disable button click
-                    $(this).click(function (event) {
-                        event.preventDefault();
-                    });
+                    $(this).off('click', AnswerClick);
                     hided_siblings = true;
                 }
             });
             // question wasn't answered - hide all answers
             if (!hided_siblings) {
                 $question.children('div.answers').css('display', 'none');
+            }
+        }
+    }
+
+    function PD(event) {
+        event.preventDefault();
+    }
+
+    function AnswerClick() {
+        var $button = $(this);
+        // if not selected before - increase select box number of questions answered
+        var increase_answered = true;
+        $($button.parent('div.answers')).children('button.answer').each(function () {
+            // if already selected
+            if ($(this).attr('value') == '1') {
+                increase_answered = false;
+            }
+        });
+        // update button pressed indicator
+        // color 'on & off' state
+        if ($button.attr('value') == '0') {
+            // cancel selected brothers and color them 'off'
+            $button.siblings('button').attr('value', '0');
+            $button.siblings('button').css({
+                'background-color': 'rgb(240, 240, 240)',
+                'font-weight': 'normal'
+            });
+            // check and color 'on'
+            $button.attr('value', '1');
+            $button.css({
+                'background-color': 'rgb(220, 220, 220)',
+                'font-weight': 'bold'
+            });
+        }
+        else if ($button.attr('value') == '1') {
+            // un check and color 'off'
+            $button.attr('value', '0');
+            $button.css({
+                'background-color': 'rgb(240, 240, 240)',
+                'font-weight': 'normal'
+            });
+        }
+
+        // if no selection at the end - reduce at select box the number of questions answered
+        var decrease_answered = true;
+        $($button.parent('div.answers')).children('button.answer').each(function () {
+            if ($(this).attr('value') == '1') {
+                decrease_answered = false;
+            }
+        });
+
+        // update questions number on select box
+        if (increase_answered || decrease_answered) {
+            var $current_subject = $('select#questions_list option[value=' + $current_question.parent('div').prop('id').substring('questions_'.length) + ']');
+            UpdateAnsweredNumber($current_subject, increase_answered, true);
+            if (window.matchMedia("(min-width: 992px)").matches) {
+                $current_subject = $('button#questions_circle_' + $current_question.parent('div').prop('id').substring('questions_'.length));
+                UpdateAnsweredNumber($current_subject, increase_answered, false);
             }
         }
     }
