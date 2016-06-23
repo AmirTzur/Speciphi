bc9a5e52061b79d1fe82a098ac5c22ea2532e95c
Elad Dan <eladdan88@gmail.com>
2016-06-23 17:26:55 +0300

    desktop: questions layout acoording to number of questions, show answered answer, fix answers layout

--- static_in_pro/our_static/js/focalization_questions.js
+++ static_in_pro/our_static/js/focalization_questions.js
@@ -81,6 +81,7 @@ $(document).ready(function () {
         // fill first indicator
         $('#question_indicators div.question_indicator:nth-child(1)').addClass('indicator_on');
     });
+
     // forward to next questions
     $('#question_nav_right').on('click', function () {
         if ($current_question.next('div').length) {
@@ -92,6 +93,7 @@ $(document).ready(function () {
             $('div#question_indicators').children('div.indicator_on').removeClass('indicator_on').next('div.question_indicator').addClass('indicator_on');
         }
     });
+
     // backward to last question
     $('#question_nav_left').on('click', function () {
         if ($current_question.prev('div').length) {
@@ -115,6 +117,13 @@ $(document).ready(function () {
                 $current_question.siblings('div.question').css('display', 'none');
                 // select last displayed questions
                 $("select#questions_list").val($current_question.parent('div.questions').prop('id').substring('questions_'.length));
+                // remove line separator
+                $('#line_separator').remove();
+                // remove side question style
+                $('div.question').removeClass('side_question');
+                $('div.answers').css('display', '');
+                $('button.answer').css('display', '');
+                $('div.questions').css('marginLeft', '');
             }
             desktop_flag = false;
         } //end mobile screens
@@ -144,26 +153,27 @@ $(document).ready(function () {
                 $('button.questions_circle').click(function (event) {
                     event.preventDefault();
                 });
-                // display pressed subject questions ( only if user didn't press on displayed subject)
+                // display pressed subject questions (only if user didn't press on displayed subject)
                 $('div.questions_desktop_container').click(function () {
-                    var pressed_subject_id = $(this).children('button').prop('id')[$(this).children('button').prop('id').length - 2];
-                    pressed_subject_id += $(this).children('button').prop('id')[$(this).children('button').prop('id').length - 1];
-                    var displayed_subject_id = $current_question.parent('div').attr('id')[$current_question.parent('div').attr('id').length - 2];
-                    displayed_subject_id += $current_question.parent('div').attr('id')[$current_question.parent('div').attr('id').length - 1];
+                    var pressed_subject_id = $(this).children('button').prop('id').substring('questions_circle_'.length);
+                    var displayed_subject_id = $current_question.parent('div').attr('id').substring('questions_'.length);
                     if (pressed_subject_id != displayed_subject_id) {
-                        if (pressed_subject_id.indexOf('_') == 0) {
-                            pressed_subject_id = pressed_subject_id[1];
-                        }
                         $('div.question').css('display', 'none');
                         $('div#questions_' + pressed_subject_id + ' div.question').css('display', 'inline-block');
                         // update current question
                         $current_question = $('div#questions_' + pressed_subject_id).children('div.question').first();
+                        StyleQuestions($current_question);
                     }
                 });
 
             } // end first desktop entry
+            // add line separator
+            $('div#questions_holder').before('<hr id="line_separator"/>');
             // show current question brothers
             $current_question.parent('div.questions').children('div.question').css('display', 'inline-block');
+            StyleQuestions($current_question);
+            // mark current subject CONTINUE IMPLEMENT
+            $('div#questions_desktop_container_' + $current_question.parent('div.questions').prop('id').substring('questions_'.length)).children().css('color', 'black');
             // update number of answered questions (from mobile select box)
             $('select#questions_list').children('option').each(function () {
                 $('button#questions_circle_' + $(this).val()).text($(this).text()[1] + $(this).text()[2] + $(this).text()[3]);
@@ -181,79 +191,128 @@ $(document).ready(function () {
         // invoke layout function
         WidthChange();
     }
-});
 
-function UpdateAnsweredNumber($subject, increase, mobile) {
-    var inner_text = $subject.text();
 
-    if (mobile) {
-        if (increase) {
-            // get text of current questions subject (select option)
-            switch (inner_text[1]) {
-                case '0':
-                    inner_text = String(inner_text).replace('(0', '(1');
-                    break;
-                case '1':
-                    inner_text = String(inner_text).replace('(1', '(2');
-                    break;
-                case '2':
-                    inner_text = String(inner_text).replace('(2', '(3');
-                    break;
-                case '3':
-                    inner_text = String(inner_text).replace('(3', '(4');
-                    break;
+    function UpdateAnsweredNumber($subject, increase, mobile) {
+        var inner_text = $subject.text();
+
+        if (mobile) {
+            if (increase) {
+                // get text of current questions subject (select option)
+                switch (inner_text[1]) {
+                    case '0':
+                        inner_text = String(inner_text).replace('(0', '(1');
+                        break;
+                    case '1':
+                        inner_text = String(inner_text).replace('(1', '(2');
+                        break;
+                    case '2':
+                        inner_text = String(inner_text).replace('(2', '(3');
+                        break;
+                    case '3':
+                        inner_text = String(inner_text).replace('(3', '(4');
+                        break;
+                }
+            } else {
+                switch (inner_text[1]) {
+                    case '1':
+                        inner_text = String(inner_text).replace('(1', '(0');
+                        break;
+                    case '2':
+                        inner_text = String(inner_text).replace('(2', '(1');
+                        break;
+                    case '3':
+                        inner_text = String(inner_text).replace('(3', '(2');
+                        break;
+                    case '4':
+                        inner_text = String(inner_text).replace('(4', '(3');
+                        break;
+                }
             }
         } else {
-            switch (inner_text[1]) {
-                case '1':
-                    inner_text = String(inner_text).replace('(1', '(0');
-                    break;
-                case '2':
-                    inner_text = String(inner_text).replace('(2', '(1');
-                    break;
-                case '3':
-                    inner_text = String(inner_text).replace('(3', '(2');
-                    break;
-                case '4':
-                    inner_text = String(inner_text).replace('(4', '(3');
-                    break;
+            // desktop
+            if (increase) {
+                // get text of current questions subject (select option)
+                switch (inner_text[0]) {
+                    case '0':
+                        inner_text = String(inner_text).replace('0/', '1/');
+                        break;
+                    case '1':
+                        inner_text = String(inner_text).replace('1/', '2/');
+                        break;
+                    case '2':
+                        inner_text = String(inner_text).replace('2/', '3/');
+                        break;
+                    case '3':
+                        inner_text = String(inner_text).replace('3/', '4/');
+                        break;
+                }
+            } else {
+                switch (inner_text[0]) {
+                    case '1':
+                        inner_text = String(inner_text).replace('1/', '0/');
+                        break;
+                    case '2':
+                        inner_text = String(inner_text).replace('2/', '1/');
+                        break;
+                    case '3':
+                        inner_text = String(inner_text).replace('3/', '2/');
+                        break;
+                    case '4':
+                        inner_text = String(inner_text).replace('4/', '3/');
+                        break;
+                }
             }
         }
-    } else {
-        // desktop
-        console.log(increase);
-        if (increase) {
-            // get text of current questions subject (select option)
-            switch (inner_text[0]) {
-                case '0':
-                    inner_text = String(inner_text).replace('0/', '1/');
-                    break;
-                case '1':
-                    inner_text = String(inner_text).replace('1/', '2/');
-                    break;
-                case '2':
-                    inner_text = String(inner_text).replace('2/', '3/');
-                    break;
-                case '3':
-                    inner_text = String(inner_text).replace('3/', '4/');
-                    break;
-            }
-        } else {
-            switch (inner_text[0]) {
-                case '1':
-                    inner_text = String(inner_text).replace('1/', '0/');
-                    break;
-                case '2':
-                    inner_text = String(inner_text).replace('2/', '1/');
-                    break;
-                case '3':
-                    inner_text = String(inner_text).replace('3/', '2/');
-                    break;
-                case '4':
-                    inner_text = String(inner_text).replace('4/', '3/');
-                    break;
+        $subject.text(inner_text);
+
+    }
+
+    function StyleQuestions($current) {
+        // if 1 questions - normal
+        // if 2 questions - 1st-cutted, 2nd-normal
+        // if 3 questions - 1st-cutted, 2nd-normal, 3rd-cutted
+        var $questions = $($current.parent('div.questions')).children('div.question');
+        switch ($questions.length) {
+            case 2:
+                // style first question
+                StyleQuestion($questions.first());
+                // center second question
+                $questions.parent().css('marginLeft', '-=215px');
+                break;
+            case 3:
+                $('div.questions').css('marginLeft', '');
+                // style first question
+                StyleQuestion($questions.first());
+                // style last question
+                StyleQuestion($questions.last());
+                break;
+            default:
+                // do nothing
+                break;
+
+        }
+        function StyleQuestion($question) {
+            $question.addClass('side_question');
+            $question.children('div.answers').addClass('side_answers');
+            // check an question was answered
+            var hided_siblings = false;
+            $($question.children('div.answers')).children('button').each(function () {
+                if ($(this).attr('value') == '1') {
+                    // hide brothers
+                    $(this).siblings('button').css('display', 'none');
+                    // disable button click
+                    $(this).click(function (event) {
+                        event.preventDefault();
+                    });
+                    hided_siblings = true;
+                }
+            });
+            // question wasn't answered - hide all answers
+            if (!hided_siblings) {
+                $question.children('div.answers').css('display', 'none');
             }
         }
     }
-    $subject.text(inner_text);
-}
+
+});
