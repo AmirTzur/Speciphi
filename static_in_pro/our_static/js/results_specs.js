723296b8c1726ee727ba75f15d19f7e25f839588
Amir Tzur <tzuramir@gmail.com>
2016-07-20 11:59:14 +0300

    Basic price slider added to the filters menu

--- static_in_pro/our_static/js/results_specs.js
+++ static_in_pro/our_static/js/results_specs.js
@@ -90,7 +90,25 @@ $(document).ready(function(){
     $('#filters-menu .checkbox input').on('change', function () {
         alert('ajax call on checkbox change');
     });
-
+    // Price Slider Filter (jQuery UI)
+    $(function() {
+        $( "#slider-range" ).slider({
+            range: true,
+            min: 200,
+            max: 10000,
+            values: [200, 10000],
+            disabled: false,
+            slide: function(event, ui){
+                $("#slider-min").val("$" + ui.values[0]);
+                $("#slider-max").val("$" + ui.values[1]);
+            },
+            stop: function(event, ui){
+                alert('Define ajax callback to get results');
+            },
+        });
+        $("#slider-min").val("$"+$("#slider-range").slider("values", 0));
+        $("#slider-max").val("$"+$("#slider-range").slider("values", 1));
+    });
 });
 
 
