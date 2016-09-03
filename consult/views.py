d06c49ca4a4d48ebc50a580a95821bebafdf4988
Elad Dan <eladdan88@gmail.com>
2016-09-03 19:57:22 +0300

    final commit

--- consult/views.py
+++ consult/views.py
@@ -16,6 +16,12 @@ import pickle
 import string
 from consult.utils import Classifier
 import pandas as pd
+from django.contrib.sessions.middleware import SessionMiddleware
+import time
+from importlib import import_module
+from django.conf import settings
+from django.utils.cache import patch_vary_headers
+from django.utils.http import cookie_date
 
 
 def home(request):
@@ -979,7 +985,8 @@ def user_actions(request):
             # if checked
             if int(action_type) == 1:
                 request.session['affiliation'].append(int(object_id))
-                # request.session.save()
+                # http://kechengpuzi.com/q/s13748166
+                request.session.save()
                 print('affiliations checked. ', request.session['affiliation'])
                 offer_list = predict('needs', classifier_ent, request)
             # if un-checked
