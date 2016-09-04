6f0d75f0a18f207b7bc854c307422c4ef276a416
Amir Tzur <tzuramir@gmail.com>
2016-09-05 01:48:16 +0300

    Fix affiliatio/application ajax sync

--- static_in_pro/our_static/js/AJAX_data_gather.js
+++ static_in_pro/our_static/js/AJAX_data_gather.js
@@ -2,11 +2,11 @@ var ajax_que = [];
 
 // affiliation
 $('#nl-type-field input').on('change', function () {
-    AJAX_manager(this, 'affiliation_choosing', true);
+    AJAX_manager(this, 'affiliation_choosing', $(this).is(':checked'), true);
 });
 // application
 $('#nl-needs-field input').on('change', function () {
-    AJAX_manager(this, 'use_ranking', true);
+    AJAX_manager(this, 'use_ranking', $(this).is(':checked'), true);
 });
 // focalization
 $('#nl-questions-field input').on('change', function () {
@@ -17,24 +17,24 @@ $('#info-elements button').on('click', function () {
     AJAX_userAction(this, 'advice_clicking');
 });
 
-function AJAX_manager(object, action_name, ajax_request) {
+function AJAX_manager(object, action_name, action_type, ajax_request) {
     if (ajax_request){
-        ajax_que.push([object, action_name]);
+        ajax_que.push([object, action_name, action_type]);
         console.log('New Request: action name - ' + action_name);
         console.log('ajax_que : ');
         console.log(ajax_que);
         if (ajax_que.length == 1){
             console.log('First element triggering AJAX');
-            AJAX_userAction(object, action_name);
+            AJAX_userAction(object, action_name, action_type);
         }
     }else {
         console.log('New Response');
         ajax_que.shift();
         console.log('Item Deleted - Update ajax_que : ');
         console.log(ajax_que);
-        if (ajax_que.length > 1) {
+        if (ajax_que.length >= 1) {
             console.log('Triggering ' + ajax_que[0][1] + ' next AJAX');
-            AJAX_userAction(ajax_que[0][0], ajax_que[0][1]);
+            AJAX_userAction(ajax_que[0][0], ajax_que[0][1], ajax_que[0][2]);
         }
     }
 }
@@ -45,20 +45,17 @@ function AJAX_manager(object, action_name, ajax_request) {
 // });
 // continue implementing closing advise and browsing advices
 
-function AJAX_userAction(object, action_name) {
-    var action_type;
+function AJAX_userAction(object, action_name, action_type_bool) {
+    var action_type = (action_type_bool) ? 1:-1;
     var object_id;
     var action_content;
     // affiliation_choosing
     if (action_name == 'affiliation_choosing') {
-        if ($(object).is(':checked')) action_type = 1;
-        else action_type = -1;
         object_id = $(object).val();
         console.log('affilliation action type: ' + action_type);
         // use_ranking
     } else if (action_name == 'use_ranking') {
-        action_type = $(object).val();
-        if (!$(object).is(':checked')) action_type = -action_type;
+        action_type = $(object).val() * action_type;
         object_id = $(object).prop('id').substr(0, $(object).prop('id').indexOf('_'));
         // question_answering
     } else if (action_name == 'question_answering') {
@@ -93,7 +90,7 @@ function AJAX_userAction(object, action_name) {
         // handle a successful response
         success: function (json) {
             // Trigger ajax
-            AJAX_manager(null, null, false);
+            AJAX_manager(null, null, null, false);
             // Update deals data
             update_deals(json['offers']);
             console.log('success'); // log the returned json to the console
@@ -140,10 +137,10 @@ function update_deals(offers) {
     });
 }
 
-// function count_matches(arr, target_val) {
-//     var count = 0;
-//     for (var i=0; i<ajax_que.length; i++){
-//
-//     }
-//     return (count%2 == 0) ? -1:1;
-// }
+function checked_factor(arr, target_obj) {
+    var count = 0;
+    for (var i=0; i<ajax_que.length; i++){
+        if (ajax_que[i][0] == target_obj) count++;
+    }
+    return (count%2 == 0) ? 1:-1;
+}
